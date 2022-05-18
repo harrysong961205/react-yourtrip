9b214f50228384f79bd3ac88f15dd80c5a58b8ab
harrysong961205 <fkdlzldeja@gmail.com>
2022-05-18 13:30:24 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default function App(props){
   return (
     <div>
       <h1>안녕하세요</h1>
-      <p>Start with your trip :)</p>
+      <p><a href="/" onClick={function(event){event.preventDefault();
+      props.onChangeMode();
+      }}>Start with your trip :)</a></p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
-function Headline(){
+const topics=[
+  {id:1, title:"여행"},
+  {id:2, title:"맛집"},
+  {id:3, title:"숙박"}
+]
+
+function Headline(props){
   return <App />
 }
 function Description(props){
@@ -12,12 +17,16 @@ function Description(props){
   </article>
 }
 function Deeplink(props){
-  const topics=[
-    {id:1, title:}
-  ]
-  return <ol>
-    <li><a href = "/read/{}">{props.title}</a></li>
+  const lis =[]
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href = {'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return<nav> 
+  <ol>
+    {lis}
   </ol>
+  </nav>
 }
 
 const rootElement = document.getElementById('root');
@@ -26,10 +35,10 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Headline></Headline>
+    <Headline onChangeMode={function(){alert('Lets go!!')}}></Headline>
     <Description title ="enjoy!" 
     body = "유어트립입니다."></Description>
-    <Deeplink title = "여행"></Deeplink>
+    <Deeplink topics={topics}></Deeplink>
 
   </StrictMode>
 );
